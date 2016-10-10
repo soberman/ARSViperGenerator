0134dc6269af2fffa3161c174188d900ca60c9aa
Yarik Arsenkin <majesticy@gmail.com>
2016-10-10 22:45:56 +0200

    Initial Commit

---
+++ ARSViperGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ARSViperGenerator
+//
+//  Created by Yaroslav Arsenkin on 10/10/2016.
+//  Copyright © 2016 Yarik Arsenkin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
